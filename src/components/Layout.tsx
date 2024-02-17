b3d5ae0aeb9316ac67fb19911a4b94cff5e4c170
Marco-Kap <kikakeks@gmx.net>
2024-02-17 21:58:51 +0100

    Update Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,52 +1,40 @@
-import { Link, Outlet } from "react-router-dom";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "react-bootstrap/Navbar";
+import { Link, Outlet } from "react-router-dom";
 
 export default function Layout() {
   return (
     <>
-      <nav className="navbar bg-light fixed-top">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="#">
-            Offcanvas navbar
-          </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="offcanvas"
-            data-bs-target="#offcanvasNavbar"
-            aria-controls="offcanvasNavbar">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div
-            className="offcanvas offcanvas-end"
-            tabIndex={-1}
-            id="offcanvasNavbar"
-            aria-labelledby="offcanvasNavbarLabel">
-            <div className="offcanvas-header">
-              <h5 className="offcanvas-title" id="offcanvasNavbarLabel">
-                Offcanvas
-              </h5>
-              <button
-                type="button"
-                className="btn-close"
-                data-bs-dismiss="offcanvas"
-                aria-label="Close"></button>
-            </div>
-            <div className="offcanvas-body">
-              <div>
-                <Link to="portfolio">Portfolio</Link>
-              </div>
-              <div>
-                <Link to="/cv">CV</Link>
-              </div>
-              <div>
-                <Link to="/">Home</Link>
-              </div>
-            </div>
-          </div>
-        </div>
-      </nav>
-
+      <Navbar
+        collapseOnSelect
+        fixed="bottom"
+        bg="dark"
+        data-bs-theme="dark"
+        expand="lg"
+        className="bg-body-tertiary">
+        <Container>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+            id="responsive-navbar-nav">
+            <Nav>
+              <Link className="" to="/resume">
+                Resume
+              </Link>
+              {"  "}
+              <Link className="" to="mailto:marco-kappeller.bewerbung@gmx.de">
+                Get in touch
+              </Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
       <Outlet />
     </>
   );
